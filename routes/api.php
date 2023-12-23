6eb710b45063eb7ec3e60ed7ea44903fd9eaef80
Rhys Devine-Davies <rhysdevine-davies@Rhyss-MacBook-Pro.local>
2023-12-23 02:58:18 +0000

    aws

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,10 @@ Route::get('/auth/resend-code', [AuthController::class, 'resendCode']);
 // https://classermedia.com/api/aws/create
 Route::post('/aws/create', [UserController::class, function (Request $request) {
     Storage::put(storage_path('logs/s3-create.log'), json_encode($request->all()));
+    return response()->json([
+        'status' => true,
+        'message' => 'success'
+    ], 200);
 }]);
 
 // User routes
