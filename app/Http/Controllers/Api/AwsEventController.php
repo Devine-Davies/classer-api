834599d7a52274c8942f155210b1e2d178b27956
Rhys Devine-Davies <rhysdevine-davies@Rhyss-MacBook-Pro.local>
2023-12-23 14:07:37 +0000

    aws

--- app/Http/Controllers/Api/AwsEventController.php
+++ app/Http/Controllers/Api/AwsEventController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Subscription;
-use App\Models\AWSEvent;
+use App\Models\AwsEvent;
 
 class AwsEventController extends Controller
 {
