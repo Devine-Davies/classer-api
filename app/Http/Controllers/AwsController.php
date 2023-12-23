dcec7208baa4dcd0db81675d8e58f32d4ddac6d1
Rhys Devine-Davies <rhysdevine-davies@Rhyss-MacBook-Pro.local>
2023-12-23 23:24:24 +0000

    aws

--- app/Http/Controllers/AwsController.php
+++ app/Http/Controllers/AwsController.php
@@ -27,7 +27,7 @@ class AwsController extends Controller
         print('Deleting files from S3');
         print_r($files);
         $s3 = Storage::disk('s3');
-        return $s3->delete($files);
+        return $s3->delete(["users/08cfd9e2-3c9b-40b2-b97e/shorts/classer-1697847956931.mp4"]);
     }
 
 
