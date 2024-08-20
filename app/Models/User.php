2d2c4e1fb58c42479f485941b89cc9618dfd9d02
Rhys Devine-Davies <rhysdevine@gmail.com>
2024-08-21 00:09:24 +0100

    sp

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,8 +43,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-        'account_status' => AccountStatus::class,
+        'password' => 'hashed'
     ];
 
     /**
