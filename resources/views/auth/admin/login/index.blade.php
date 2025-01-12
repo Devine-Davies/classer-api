a8a56756059b1098e38deee6ba51c82a70a700cd
Rhys Devine-Davies <Devine-Davies@users.noreply.github.com>
2025-01-12 23:10:43 +0000

    sp

--- resources/views/auth/admin/login/index.blade.php
+++ resources/views/auth/admin/login/index.blade.php
@@ -3,16 +3,13 @@
 
 <head>
     <title>Classer - Login</title>
-
     <script>
         pageUrl = "{{ url('/') }}";
     </script>
 
     @include('partials.shared.meta')
-    @vite('resources/css/markdown/main.css')
-    @vite('resources/views/admin/login/index.css')
-    @vite('resources/views/auth/admin/login/index.js')
+
 </head>
 
-
+</html>
 
