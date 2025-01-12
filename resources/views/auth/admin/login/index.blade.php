db4f72c58234219539d8419929c72d20fa9865e8
Rhys Devine-Davies <Devine-Davies@users.noreply.github.com>
2025-01-12 23:07:45 +0000

    sp

---
+++ resources/views/auth/admin/login/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Classer - Login</title>
+
+    <script>
+        pageUrl = "{{ url('/') }}";
+    </script>
+
+    @include('partials.shared.meta')
+    @vite('resources/css/markdown/main.css')
+    @vite('resources/views/admin/login/index.css')
+    @vite('resources/views/auth/admin/login/index.js')
+</head>
+
+
+
