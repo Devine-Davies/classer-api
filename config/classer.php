bc05c806eaaf34b2ebbbe7d4a9bce0f125665f87
Rhys Devine-Davies <Devine-Davies@users.noreply.github.com>
2025-08-22 00:13:01 +0100

    removed vendor files and added missing files

--- config/classer.php
+++ config/classer.php
@@ -1,15 +1,37 @@
 <?php
 
 return [
-    // Application name
+    /**
+     * Admin email addresses for notifications
+     */
     'admin_email' => env('APP_ADMIN_EMAILS', 'admin@example.com,support@example.com'),
 
-    // Cloud Share configuration
+    /**
+     * Scheduler configuration
+     */
+    'scheduler' => [
+        'mail' => [
+            'command' => 'queue:work --queue=mail --once',
+            'expression' => env('CRON_EXPRESSION_MAIL', '* * * * *'), // Every minute
+        ],
+        'cloudShareVerify' => [
+            'command' => 'queue:work cloudshare --queue=verify --once',
+            'expression' => env('CRON_EXPRESSION_CLOUD_SHARE_VERIFY', '0 */4 * * *'), // Every 4 hours
+        ],
+        'cloudShareExpire' => [
+            'command' => 'queue:work cloudshare --queue=expire --once',
+            'expression' => env('CRON_EXPRESSION_CLOUD_SHARE_EXPIRE', '0 0 * * *'), // Daily at midnight
+        ],
+    ],
+
+    /**
+     * Cloud Share configuration
+     */
     'cloudShare' => [
         'directory' => env('CLOUD_SHARE_DIRECTORY', 'cloud-share'),
-        'put_object_timeout' => env('CLOUD_SHARE_S3_PUT_OBJECT_TIMEOUT', '+1 minute'),
-        'get_object_timeout' => env('CLOUD_SHARE_S3_GET_OBJECT_TIMEOUT', '+2 minutes'),
-        'verify_delay' => env('CLOUD_SHARE_VERIFY_DELAY', '+1 minute'),
+        'putObjectTimeout' => env('CLOUD_SHARE_S3_PUT_OBJECT_TIMEOUT', '+1 minute'),
+        'getObjectTimeout' => env('CLOUD_SHARE_S3_GET_OBJECT_TIMEOUT', '+2 minutes'),
+        'verifyDelay' => env('CLOUD_SHARE_VERIFY_DELAY', '+1 minute'),
         'expire_after' => env('CLOUD_SHARE_EXPIRE_AFTER', '+2 minutes'),
     ],
 ];
